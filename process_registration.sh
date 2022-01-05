aeca99197754f98e8ecb56431d0fdd9553abde0c
rickymwalsh <rickymwalsh@gmail.com>
2022-01-05 12:07:49 +0000

    Tidy up script

--- process_registration.sh
+++ process_registration.sh
@@ -2,6 +2,7 @@ mkdir final intermediate patches;
 
 cd intermediate;
 
+##### Process T2* image - crop around spinal cord & convert to isotropic dimensions. ###########
 # Detect the centerline of the spinal cord.
 sct_get_centerline -i ../SC/t2sMerge.nii.gz -c t2s
 
@@ -15,16 +16,19 @@ sct_crop_image -i ../SC/t2sMerge.nii.gz -m mask_t2sMerge.nii.gz -o t2sMerge_crop
 # Resample the cropped t2s image to isotropic voxel dimensions of size 0.5mm
 sct_resample -i t2sMerge_crop.nii.gz -mm 0.5x0.5x0.5 -o ../final/t2sMerge_iso.nii.gz
 
+##### Process lesion mask - binarise, crop & convert to isotropic dimensions. ###########
 # Binarise the lesion masks. Originally they are the labelled with the number of the lesion.
 animaThrImage -i ../SC/labelLesion.nii.gz -o labelLesion_bin.nii.gz -t 0.5
 
-# Map the t2 image into cropped isotropic T2* image. This converts the T2 to isotropic and crops it to the same region.
-animaApplyTransformSerie -i ../SC/t2.nii.gz -g ../final/t2sMerge_iso.nii.gz -o ../final/t2_iso.nii.gz -t ~/mri/tools/identityTr.xml
 # Do the same for the image with lesion masks (already in T2* space, but want to convert to isotropic and crop).
 animaApplyTransformSerie -i labelLesion_bin.nii.gz -g ../final/t2sMerge_iso.nii.gz -o labelLesion_iso.nii.gz -t ~/mri/tools/identityTr.xml
 
 # The above transformation causes interpolation and values between 0 and 1. Threshold the lesion masks to obtain binary values again.
 animaThrImage -i labelLesion_iso.nii.gz -o ../final/labelLesion_iso_bin.nii.gz -t 0.5
+
+##### Process T2 image - crop around spinal cord & convert to isotropic dimensions. ###########
+# Map the t2 image to the cropped isotropic T2* image. This converts the T2 to isotropic and crops it to the same region.
+animaApplyTransformSerie -i ../SC/t2.nii.gz -g ../final/t2sMerge_iso.nii.gz -o ../final/t2_iso.nii.gz -t ~/mri/tools/identityTr.xml
 
 # Create a binary mask specifying where the intensities are zero or non-zero. This will be used to specify the region used to calculate the transformations below.
 animaThrImage -i ../final/t2sMerge_iso.nii.gz -o t2sMerge_iso_mask.nii.gz -t 0.0
